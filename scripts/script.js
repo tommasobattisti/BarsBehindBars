32e8b10793b8af41daafb18e41b93c642c9a3fe2
Tommaso Battisti <82956419+tommasobattisti@users.noreply.github.com>
2023-04-10 13:04:40 +0200

    Update tables section: qualitative analysis

--- scripts/script.js
+++ scripts/script.js
@@ -9,4 +9,33 @@ $(document).ready(function() {
             $('#mobile-menu-btn').html('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/></svg>')
         };
     });
+
+    $('#quality-btn').click(function() {
+        activateAnalysisSection('quality');
+    });
+
+    $('#legal-btn').click(function() {
+        activateAnalysisSection('legal');
+    });
+
+    $('#ethical-btn').click(function() {
+        activateAnalysisSection('ethical');
+    });
+
+    $('#technical-btn').click(function() {
+        activateAnalysisSection('technical');
+    });
+
 });
+
+
+
+function activateAnalysisSection(section) {
+    $('.analysis-container').addClass('hidden');
+    $('#' + section + '-analysis').removeClass('hidden');
+
+    $('.analysis-h3').removeClass('active-analysis');
+    $('.analysis-spin').addClass('hidden');
+    $('#' + section + '-h3').addClass('active-analysis');
+    $('#' + section + '-spin').removeClass('hidden');
+}
